0e37005534bd707a04a52c7882488685c9420beb
Juan Manuel <juanmanuel.manuel70@gmail.com>
2022-09-12 20:28:41 -0300

    ajustes css perfil

--- src/views/Profile/Profile.js
+++ src/views/Profile/Profile.js
@@ -1,20 +1,20 @@
-import axios from 'axios';
-import React, { useEffect } from 'react'
-import { useState } from 'react';
-import { httpGet } from '../../components/utils/httpFunctions';
-import Button from 'react-bootstrap/esm/Button';
-import { useNavigate } from 'react-router-dom';
+import axios from "axios";
+import React, { useEffect } from "react";
+import { useState } from "react";
+import { httpGet } from "../../components/utils/httpFunctions";
+import Button from "react-bootstrap/esm/Button";
+import { useNavigate } from "react-router-dom";
+import "./Profile.css";
 
 const Profile = () => {
   const navigate = useNavigate();
   const [profile, setProfile] = useState({});
-  useEffect(()=>{
-    httpGet("api/auth/me/").then((res)=>{
+  useEffect(() => {
+    httpGet("api/auth/me/").then((res) => {
       setProfile(res.data);
       console.log(res.data);
-    })
-  },[]);
- 
+    });
+  }, []);
 
   const logOut = () => {
     localStorage.clear();
@@ -23,19 +23,118 @@ const Profile = () => {
   };
 
   return (
-    <div className='container'>
-      <h3>PERFIL</h3>
-      <div>
-        <p>DNI: {profile.username}</p>
-        <p>Email: {profile.email}</p>
-        <p>Nombre: {profile.first_name}</p>        
-        <p>Apellido: {profile.last_name}</p>              
-      </div>
-      <div style={{ margin: "2vw"}} onClick={logOut}>
-      <Button>Cerrar Sesión</Button> 
-      </div>
+    <div>
+      <h1 className="h1-profile">
+        <a style={{ textDecoration: "none", color: "black" }} href="/Perfil">
+          Perfil
+        </a>
+      </h1>
+      <h1 className="h1-profile">
+        <a style={{ textDecoration: "none", color: "black" }} href="/Rutina">
+          Rutina
+        </a>
+      </h1>
+      {profile.is_staff == true ? (
+        <h1 className="h1-profile">
+          <a
+            style={{ textDecoration: "none", color: "black" }}
+            href="/Registro"
+          >
+            Crear usuario
+          </a>
+        </h1>
+      ) : null}
+      <h1 className="h1-profile">
+        <a style={{ textDecoration: "none", color: "red" }} href="/">
+          Cerrar sesion
+        </a>
+      </h1>
+      <form className="container-profile">
+        <div className="form-group">
+          <div className="input-profile">
+            <div className="w-full py-5 ">
+              <label
+                className="material-input border-gray-400
+    material-input-focused
+    "
+              >
+                <span className="input-title">Nombre</span>
+                <br></br>
+                <input
+                  type="text"
+                  className="w-full pb-1 pl-1 bg-transparent focus:outline-none focus:shadow-none"
+                  name="nombre"
+                  defaultValue={profile.first_name}
+                />
+              </label>
+            </div>
+          </div>
+          <div className="input-profile">
+            <div className="w-full py-5 ">
+              <label
+                className="material-input border-gray-400
+    material-input-focused
+    "
+              >
+                <span className="input-title">Apellido</span>
+                <br></br>
+                <input
+                  type="text"
+                  className="w-full pb-1 pl-1 bg-transparent focus:outline-none focus:shadow-none"
+                  name="apellidoPaterno"
+                  defaultValue={profile.last_name}
+                />
+              </label>
+            </div>
+          </div>
+          <div className="input-profile"></div>
+          <div className="input-profile">
+            <div className="w-full py-5 ">
+              <label
+                className="material-input border-gray-400
+    material-input-focused
+    "
+              >
+                <span className="input-title">DNI</span>
+                <br></br>
+                <input
+                  disabled
+                  type="text"
+                  className="w-full pb-1 pl-1 bg-transparent focus:outline-none focus:shadow-none"
+                  name="dni"
+                  value={profile.username}
+                />
+              </label>
+            </div>
+          </div>
+          <div className="input-profile">
+            <div className="w-full py-5 ">
+              <label
+                className="material-input border-gray-400
+    material-input-focused
+    "
+              >
+                <span className="input-title">Correo electronico</span>
+                <br></br>
+                <input
+                  type="text"
+                  className="w-full pb-1 pl-1 bg-transparent focus:outline-none focus:shadow-none"
+                  name="mail"
+                  defaultValue={profile.email}
+                />
+              </label>
+            </div>
+          </div>
+          <div className="w-full mb-10 text-center md:w-2/5"></div>
+          <div className="w-full mb-10 text-center md:w-2/5">
+            <button type="submit" className="orange-pill-button">
+              Guardar
+            </button>
+          </div>
+        </div>
+      </form>
     </div>
-  )
-}
+  );
+};
 
-export default Profile
+export default Profile;
