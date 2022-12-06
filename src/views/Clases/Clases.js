c4540c461ab1ddfce13a5e9e07f35222e7933321
Juan Manuel <juanmanuel.manuel70@gmail.com>
2022-12-06 17:39:20 -0300

    added routine for the user

--- src/views/Clases/Clases.js
+++ src/views/Clases/Clases.js
@@ -4,20 +4,28 @@ import { httpGet } from "../../components/utils/httpFunctions";
 const Clases = () => {
 
     const [user, setUser] = useState({});
+    const [routine, setRoutine] = useState({});
 
     useEffect(()=>{
       httpGet("api/auth/me/").then((res) => {
         setUser(res.data);
         
         httpGet("api/routines/?user="+res.data.id).then((res) => {
+          setRoutine(res.data);
           console.log(res.data);
         });
       });
     },[]);
 
-
   return (
-    <div>Rutinas</div>
+    <div>
+      <h1>Rutina</h1>
+
+      <h3>{routine !== undefined && routine.length > 0 && routine[1].routine_days[0].exercises[0].exercise.name}</h3>
+      <h4>Repeticiones: {routine !== undefined && routine.length > 0 && routine[1].routine_days[0].exercises[0].reps}</h4>
+      <h4>Series: {routine !== undefined && routine.length > 0 && routine[1].routine_days[0].exercises[0].sets}</h4>
+
+    </div>
   )
 }
 
